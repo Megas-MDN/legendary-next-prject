35fe68bea2c44056b3e888c2482089c6adb2dc7e
Megas Melquisedeque <mdn.melqui@gmail.com>
2024-12-07 21:02:59 -0300

    feat: Simple line fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
-    <div className="text-2xl">
-      Olá my Queen
-      <span role="img" aria-label="coracao">
-        ❤️
-      </span>{" "}
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl">
+        Olá my Queen
+        <span role="img" aria-label="coracao">
+          ❤️
+        </span>{" "}
+      </h1>
     </div>
   );
 }
